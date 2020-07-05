96bf432ac52367f4964a40a10877ce50ab238ca0
ryantoddgarza <ryantoddgarza@gmail.com>
2020-07-05 16:40:51 -0700

    add autoprefixer to develop config

--- boilerplates/webpack-react-scss/webpack.dev.js
+++ boilerplates/webpack-react-scss/webpack.dev.js
@@ -1,5 +1,6 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js')
+const autoprefixer = require('autoprefixer');
 
 module.exports = merge(common, {
   mode: 'development',
@@ -26,6 +27,15 @@ module.exports = merge(common, {
         use: [
           'style-loader',
           'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                autoprefixer({})
+              ]
+            }
+          },
           {
             loader: 'sass-loader',
             options: {
